9286700f601d4e774e6be71e822375726e7f0812
UrayMR <umrajsyafiq@gmail.com>
2026-06-10 08:28:33 +0700

    feat: add CORS middleware to allow requests from specified origins

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import api
 
 app = FastAPI()
 app.include_router(api.router)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
